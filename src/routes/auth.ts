e0a1c3fda7e33e57a5a7685bfe9e50201a2340e1
gideonadeti <gideonadeti0@gmail.com>
2024-09-29 11:08:46 +0000

    Set up Google authentication routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { handleSignUpPost, handleRefreshTokenPost, handleSignInPost } from "../controllers/auth";
+
+import {
+  handleSignUpPost,
+  handleRefreshTokenPost,
+  handleSignInPost,
+  handleGoogleCallbackGet,
+} from "../controllers/auth";
+import google from "../middlewares/google";
+import googleCallback from "../middlewares/google-callback";
 
 const router = Router();
 
 router.post("/sign-up", handleSignUpPost);
 router.post("/refresh-token", handleRefreshTokenPost);
 router.post("/sign-in", handleSignInPost);
+
+router.get("/google", google);
+router.get("/google/callback", googleCallback, handleGoogleCallbackGet);
 
 export default router;
